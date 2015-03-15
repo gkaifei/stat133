b95d29ab139fafd2b8a3cab31ad5542e1faac69c
Kaifei Guo <guokaifei@berkeley.edu>
2015-03-15 16:38:57 -0700

    homework 5

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -8,10 +8,11 @@
 ## that stores the state of the system (i.e. location of red and blue cars)
 
 bml.init <- function(r, c, p){
-
-  
-   #return(m)
+  data_sample <- sample(0:2, prob=c(1-p,p/2,p/2), size = r*c, replace = TRUE)
+  m <- matrix(data = data_sample, nrow = r, ncol= c, byrow = FALSE)
+  return(m)
 }
+
 
 #### Function to move the system one step (east and north)
 ## Input : a matrix [m] of the same type as the output from bml.init()
@@ -22,15 +23,44 @@ bml.init <- function(r, c, p){
 ## you can write extra functions that do just a step north or just a step east.
 
 bml.step <- function(m){
-
+  m.original <- m
+  m.1 <- m[ , c(ncol(m), 1:ncol(m)-1)]
+  m.position1 <- (m == 0 & m.1 == 1) * 1
+  m.moving1 <- m + m.position1
+  m.position1.moved <- m.position1 [ ,c(2:ncol(m.position1),1)]
+  m <- m.moving1 - m.position1.moved
   
-   #return(list(m, grid.new))
+  m.2 <- m[c(2:nrow(m),1), ]
+  m.position2 <- (m == 0 & m.2 == 2) * 2
+  m.moving2 <- m + m.position2
+  m.position2.moved <- m.position2[c(nrow(m.position2),1:nrow(m.position2)-1), ]
+  m <- m.moving2 - m.position2.moved
+  
+  grid.new <- any(m != m.original)
+   return(list(m, grid.new))
 }
+
+
 
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
-bml.sim <- function(r, c, p){
-
+bml.sim <- function(r,c,p){
+  m <- bml.init(r,c,p)
+  i <- 1
+  while (i <= 1000){
+    m <- bml.step(m)[[1]]
+    grid.new <- bml.step(m)[[2]]
+    if (grid.new == FALSE){
+      summary <- list("Gridlocked Matrix" =m, "Total Steps" = i, "Traffic Density" = p)
+      return(summary)
+      break}
+    else {i <- i + 1}
+  }
+   return(list("Free Flowing Matrix" = m, "Total Steps" = i-1, "Traffic Density" = p))   
 }
+
+
+
+
--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -7,4 +7,91 @@
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
 
+probs <- c(0.1,0.3,0.5,0.6,0.7,0.8,0.9)
 
+## Returns matrix that lists timestep count before Gridlock or Freeflow limit for 1000 trials
+## for each density in the traffic model. Note: p can take ina vector of probabilities.
+SimData <- function(r,c){
+  TimeStepMat <- matrix(ncol=length(probs), nrow = 1000)
+  for (i in 1:length(probs)){
+    TimeStepMat[,i] <- replicate(1000, bml.sim(r,c,probs[i])[[2]])}
+  return(TimeStepMat)
+  }
+
+## Returns a vector that lists the average timestpes it took for a certain car density in the
+## traffic model for those triasl that hit gridlock before 1000 timesteps.
+MeanGridlock <- function(m){
+  MeanGLs <- c()
+  for(i in 1:ncol(m)){
+    GLMean <- mean(m[m[,i]!=1000,i])
+    MeanGLs <- cbind(MeanGLs,GLMean)
+    colnames(MeanGLs)[[i]] <- c(paste("p=",probs[[i]], sep=""))
+    rownames(MeanGLs)[[1]] <- c("Average GL Timesteps")
+  }
+  return(MeanGLs)
+}
+
+## Returns a matrix that lists the frequency that a certain car density in the traffic model would
+## hit gridlock and maintain free-flow
+
+TSFreq <- function(m){
+  Gridlock_Frequency <- c()
+  Free_Flow_Frequency <- c()
+  for(i in 1:ncol(m)){
+    freqGL <- length(m[m[,i]!=1000,i])/nrow(m)
+    Gridlock_Frequency <- cbind(Gridlock_Frequency,freqGL)
+    freqFF <- length(m[m[,i]==1000,i])/nrow(m)
+    Free_Flow_Frequency <- cbind(Free_Flow_Frequency,freqFF)
+  }
+  Freqs <- rbind(Gridlock_Frequency,Free_Flow_Frequency)
+  for(i in 1:ncol(m)){
+    colnames(Freqs)[i] <- c(paste("p=",probs[[i]],sep=""))
+  }
+  rownames(Freqs) <- c("Gridlock Frequency","Free Flow Frequency")
+  return(Freqs)
+}
+
+
+
+## 10x10 Matrix
+TenDataMat <- SimData(10,10)
+TenDataMat
+save(TenDataMat, file="TenDataMat.RData")
+
+## 20x20 Matrix
+TwentyDataMat <- SimData(20,20)
+TwentyDataMat
+save(TwentyDataMat, file="TwentyDataMat.RData")
+
+## 50x50 Matrix
+FiftyDataMat <- SimData(50,50)
+FiftyDataMat
+save(FiftyDataMat, file= "FiftyDataMat.RData")
+
+## Return the average steps according to P (10 by 10)
+Ave.Mean.10by10 <- MeanGridlock(TenDataMat)
+Ave.Mean.10by10
+save(Ave.Mean.10by10, file= "Ave.Mean.10by10.RData")
+barplot(Ave.Mean.10by10, xlab="probs", ylab= "Average Steps Taken", main = "Average Steps Taken vs Probs 10*10")
+
+##
+TSFreq(TenDataMat)
+
+## Return the average steps according to P (20 by 20)
+Ave.Mean.20by20 <- MeanGridlock(TwentyDataMat)
+Ave.Mean.20by20
+save(Ave.Mean.20by20, file= "Ave.Mean.20by20.RData")
+barplot(Ave.Mean.20by20, xlab="probs", ylab= "Average Steps Taken", main = "Average Steps Taken vs Probs 20*20")
+
+##
+TSFreq(TwentyDataMat)
+
+## Return the average steps according to P (50 by 50)
+Ave.Mean.50by50 <- MeanGridlock(TenDataMat)
+Ave.Mean.50by50
+save(Ave.Mean.50by50, file= "Ave.Mean.50by50.RData")
+barplot(Ave.Mean.50by50, xlab="probs", ylab= "Average Steps Taken", main = "Average Steps Taken vs Probs 50*50")
+
+##
+##
+TSFreq(FiftyDataMat)
